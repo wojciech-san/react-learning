c91d4bca8416b2db213e605380e07995fabe590a
Wojciech <wojciech.czok@gmail.com>
2026-07-26 20:58:53 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hello from "./components/Hello";
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hello.jsx
@@ -0,0 +1,9 @@
+function Hello() {
+  return (
+    <div>
+      <h1>Hello Wojciech!</h1>
+    </div>
+  );
+}
+
+export default Hello;
