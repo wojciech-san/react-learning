d2c0b458ecab404527c5748fc80d15dcd3e2cef5
Wojciech <wojciech.czok@gmail.com>
2026-08-01 17:31:51 +0200

    finished 27

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 
 export default function Form() {
   const [name, setName] = useState({ firstName: "", lastName: "" });
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(name);
+  }
   return (
     <div>
       {name.firstName}-{name.lastName}
@@ -16,6 +20,7 @@ export default function Form() {
           type="text"
           value={name.lastName}
         />
+        <button onClick={(e) => handleSubmit(e)}>Add</button>
       </form>
     </div>
   );
